e731aa9972474e8fde98cb620933a79753ca6550
Ah Shum <ahshum911@gmail.com>
2017-11-27 07:59:06 +0800

    Update win10.sh

--- win10.sh
+++ win10.sh
@@ -5,8 +5,9 @@ OPT="$OPT -enable-kvm"
 
 # Core
 OPT="$OPT -cpu host"
-OPT="$OPT -M q35"
+OPT="$OPT -smp 4,cores=2,threads=2,sockets=1"
 OPT="$OPT -m 4G"
+OPT="$OPT -M q35"
 
 # Graphic
 OPT="$OPT -vga qxl"
@@ -17,9 +18,15 @@ OPT="$OPT -soundhw ac97"
 # Drive
 OPT="$OPT -drive file=img/base.img,media=disk,index=0"
 OPT="$OPT -drive file=/home/ahshum/Downloads/Windows10.iso,media=cdrom"
+#OPT="$OPT -boot order=dc"
 
 # Net
 OPT="$OPT -net nic,model=e1000"
 OPT="$OPT -net user,smb=/vm/win10/shared"
 
+# Pass through
+#OPT="$OPT -usb -usbdevice host:046d:c531"
+
+# Boot
+export QEMU_AUDIO_DRV=alsa
 qemu-system-x86_64 $OPT $@
