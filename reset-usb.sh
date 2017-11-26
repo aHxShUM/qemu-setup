74ac28511f2a5d372d082c723e08dbbdc9dd765d
Ah Shum <ahshum911@gmail.com>
2017-11-26 14:06:52 +0800

    Update reset-usb.sh

--- reset-usb.sh
+++ reset-usb.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
 
+# Notes: [1]: the *hci_hcd kernel drivers typically control the USB ports.
+# ohci_hcd and uhci_hcd are for USB1.1 ports,
+# ehci_hcd is for USB2 ports and
+# xhci_hcd is for USB3 ports.
+
 for i in /sys/bus/pci/drivers/[uoex]hci_hcd/*:*; do
   echo "${i##*/}" > "${i%/*}/unbind"
   echo "${i##*/}" > "${i%/*}/bind"
