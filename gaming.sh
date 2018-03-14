c10d99ebee3f5dc4efbfa7bfd9d1d79654d9c96c
Ah Shum <ahshum911@gmail.com>
2018-03-14 09:25:36 +0800

    Update gaming.sh

--- gaming.sh
+++ gaming.sh
@@ -8,24 +8,25 @@ OPT="$OPT -rtc base=localtime"
 OPT="$OPT -alt-grab"
 
 # Core
-#OPT="$OPT -cpu host,kvm=off,hv_time,hv_relaxed,hv_vapic,hv_spinlocks=0x1fff,hv_vendor_id=123456789ab"
-OPT="$OPT -cpu host,kvm=off,hv_vendor_id=123456789ab"
+#OPT="$OPT -cpu host,kvm=off,hv_time,hv_relaxed,hv_vapic,hv_spinlocks=0x1fff,hv_vendor_id=Nvidia43FIX"
+OPT="$OPT -cpu host,kvm=off,hv_vendor_id=123456789ab,hv_relaxed,hv_spinlocks=0x1fff,hv_vapic,hv_time"
 OPT="$OPT -smp cores=4,threads=1,sockets=1"
-OPT="$OPT -m 6G"
+OPT="$OPT -m 8G"
 OPT="$OPT -M q35"
 OPT="$OPT -bios /usr/share/qemu/OVMF.fd"
 
 # Display
-OPT="$OPT -vga qxl"
+OPT="$OPT -vga std"
 
 # Sound
 OPT="$OPT -device AC97"
 
 # Drive
 #OPT="$OPT -boot order=dc"
-OPT="$OPT -drive file=/vm/gaming/img/gaming.img,if=virtio,format=qcow2"
-OPT="$OPT -drive file=/home/ahshum/Downloads/Windows10.iso,media=cdrom"
-OPT="$OPT -drive file=/home/ahshum/Downloads/virtio-win-0.1.141.iso,media=cdrom"
+OPT="$OPT -drive file=/vm/gaming/img/gaming.img,if=virtio,format=raw"
+#OPT="$OPT -drive file=/home/ahshum/Downloads/Windows10.iso,media=cdrom"
+#OPT="$OPT -drive file=/home/ahshum/Downloads/virtio-win-0.1.141.iso,media=cdrom"
+OPT="$OPT -drive file=/dev/sdd,format=raw"
 
 # Net
 OPT="$OPT -netdev bridge,id=br0"
